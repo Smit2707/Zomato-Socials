0ec723ca1912060ff379df2955d227e3c697bf45
Smit Yadav <Smityadav2707@gmail.com>
2025-09-03 19:59:27 +0530

    Add UUID dependency and implement food creation logic with file upload using imagekit

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -1,10 +1,26 @@
 const foodModel = require("../models/food.model");
+const { uploadFile } = require("../services/storage.service");
+const { v4: uuid } = require("uuid")
 
 const createfood = async (req, res) => {
     const partner = req.partner;
-    res.send("food created successfully.");
+    console.log(partner)
     console.log(req.body);
     console.log(req.file);
+
+    const fileUpload = await uploadFile(req.file.buffer, uuid());
+
+    const foodItem = await foodModel.create({
+        name: req.body.name,
+        video: fileUpload.url,
+        description: req.body.descreption,
+        foodPartner: partner._id
+    });
+
+    res.status(201).json({
+        message: "Food Item Added Successfully.",
+        food : foodItem
+    })
 }
 
 module.exports = createfood;
