9e6b47bf580cf827819765a55baa1b76c8ea2051
Smit Yadav <Smityadav2707@gmail.com>
2025-09-07 08:15:09 +0530

    ui changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -132,20 +132,20 @@ const Home = () => {
               </Link>
 
               {/* Engagement Metrics */}
-              {/* <div className="flex justify-between text-gray-300 text-sm">
+              <div className="flex justify-between text-gray-300 text-sm">
                 <div className="flex items-center">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                   </svg>
-                  <span>{reel.likes.toLocaleString()}</span>
+                  <span>{reel?.likes || 30}</span>
                 </div>
                 <div className="flex items-center">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                   </svg>
-                  <span>{reel.comments.toLocaleString()}</span>
+                  <span>{reel?.comments || 2}</span>
                 </div>
-              </div> */}
+              </div>
             </div>
 
             {/* Right Side Actions */}
@@ -179,7 +179,7 @@ const Home = () => {
       </div>
 
       {/* Bottom Navigation */}
-      {/* <nav className="fixed bottom-0 left-0 right-0 bg-black flex justify-around items-center p-3 border-t border-gray-800">
+      <nav className="fixed bottom-0 left-0 right-0 bg-black flex justify-around items-center p-3 border-t border-gray-800">
         <button className="text-white flex flex-col items-center">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
@@ -213,7 +213,7 @@ const Home = () => {
           </svg>
           <span className="text-xs mt-1">Profile</span>
         </button>
-      </nav> */}
+      </nav>
     </div>
   );
 };
